0cd4717a55ce5188e7255e6205b5f8898d70a8b2
raulvillalobosjr <49546176+raulvillalobosjr@users.noreply.github.com>
2021-04-07 17:37:16 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='1.0',
+      description='A math module',
+      author='Raul Villalobos',
+      author_email='raulvillajr95@gmail.com',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['distutils', 'distutils.command'],
+     )
